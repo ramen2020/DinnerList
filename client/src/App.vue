49af8f4df4a4e1ccd4a4719f547b24448d254d26
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-07-26 14:03:12 +0900

    add_client_App.vue追加

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <Navbar />
+    <v-content>
+      <RouterView />
+    </v-content>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+
+</script>
+
+<style>
+
+</style>
